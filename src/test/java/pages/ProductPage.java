413f32968614d2fbad6cd4785dd5b336af672e16
basitaziz <basitaziz7@gmail.com>
2025-07-09 16:12:15 +0500

    fix

--- src/test/java/pages/ProductPage.java
+++ src/test/java/pages/ProductPage.java
@@ -25,7 +25,12 @@ public class ProductPage {
     }
 
     public void addFirstProductToCart() {
-        
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         //wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("notification-container")));
         // Wait until the element is *visible*, not just present
